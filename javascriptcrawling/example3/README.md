24f50ece0fab700d1e66c30314d4fb7a36d51798
hklee <leehunkyu00@gmail.com>
2019-06-14 13:37:52 +0900

    Added sample using request, cheerio-httpcli.

---
+++ javascriptcrawling/example3/README.md
@@ -0,0 +1,5 @@
+## request 모듈 사용
+
+request.js : html download
+download.js : 이미지 다운로드
+downloadRec.js : 재귀적으로 html 다운로드하는 것
---
+++ javascriptcrawling/example3/downloadRec.js
@@ -0,0 +1,59 @@
+const client = require('cheerio-httpcli');
+const request = require('request');
+const urlType = require('url');
+const fs = require('fs');
+const path = require('path');
+
+const LINK_LEVEL = 3;
+const TARGET_URL = "https://nodejs.org/dist/latest-v10.x/docs/api/";
+let list = {};
+
+downloadRec(TARGET_URL, 0);
+
+function downloadRec(url, level) {
+    if (level >= LINK_LEVEL) return;
+    if (list[url]) return;
+
+    list[url] = true;
+
+    // 외부 페이지는 무시한다.
+    let us = TARGET_URL.split("/");
+    us.pop();
+    const base = us.join("/");
+    if (url.indexOf(base) < 0) return;
+
+    // get HTML
+    client.fetch(url, {}, function(err, $, res) {
+        $("a").each(function(idx) {
+            let href = $(this).attr('href');
+            if (!href) return;
+
+            href = urlType.resolve(url, href);
+
+            href = href.replace(/\#.+$/, "");
+            downloadRec(href, level + 1);
+        });
+
+        if (url.substr(url.length - 1, 1) == '/') {
+            url += "index.html";
+        }
+
+        const savepath = url.split('/').slice(2).join("/");
+        checkSaveDir(savepath);
+        console.log(savepath);
+        fs.writeFileSync(savepath, $.html());
+    });
+}
+
+function checkSaveDir(fname) {
+    const dir = path.dirname(fname);
+
+    dirlist = dir.split("/");
+    let p = "";
+    for (let i in dirlist) {
+        p += dirlist[i] + "/";
+        if (!fs.existsSync(p)) {
+            fs.mkdirSync(p);
+        }
+    }
+}
